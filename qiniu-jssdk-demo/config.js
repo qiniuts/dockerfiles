fb1f046c139e95f41c947d7a6e0051e286d0575c
rwifeng <rw.ifeng@gmail.com>
2017-04-23 14:30:44 +0800

    init qiniu jssdk demo

---
+++ qiniu-jssdk-demo/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'ACCESS_KEY': '<your_access_key>',  // https://portal.qiniu.com/user/key
+    'SECRET_KEY': '<your_secret_key>',
+    'Bucket_Name': '<your_bucket_name>',
+    'Port': 80,
+    'Uptoken_Url': 'uptoken',
+    'Domain': 'http://<your_bucket_domain>/' // bucket domain eg:http://qiniu-plupload.qiniudn.com/
+};
